d6ee403a2d569592d9bc95f47abb3a1d73623046
Wojciech <wojciech.pastuszka@gazeta.pl>
2017-06-06 20:17:57 +0200

    init React app

---
+++ public/app.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const Greeter = require('Greeter');
+
+ReactDOM.render(<Greeter />, document.getElementById("app"));
---
+++ public/components/Greeter.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+
+const Greeter = () => { return <div>Here comes text from Greeter component</div>};
+
+module.exports = Greeter;
